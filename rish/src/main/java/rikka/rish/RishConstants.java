92ecd5531a27d73b5a17201c86c1012fc44f918c
you573166-hub <you573166@gmail.com>
2025-07-26 22:40:21 +0700

    Delete rish directory

--- rish/src/main/java/rikka/rish/RishConstants.java
+++
@@ -1,8 +0,0 @@
-package rikka.rish;
-
-class RishConstants {
-
-    public static final int ATTY_IN = (1);
-    public static final int ATTY_OUT = (1 << 1);
-    public static final int ATTY_ERR = (1 << 2);
-}
